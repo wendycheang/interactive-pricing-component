438704ebe1bfb1fb79c98ab2e9474e8a4c79cd56
wendycheang <wendy_13@mit.edu>
2023-08-28 21:47:59 -0400

    test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const WidgetDivs = document.querySelectorAll('.ray_widget')
+
+WidgetDivs.forEach(divElement => {
+  const root = ReactDOM.createRoot(divElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+})
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path")
+const TerserPlugin = require("terser-webpack-plugin");
+const glob = require("glob")
+
+module.exports = {
+  entry: {
+    "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
+  },
+  output: {
+    filename: "build/static/js/bundle.min.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+}
